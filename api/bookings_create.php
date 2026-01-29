71560cb72ea56f3bed01762a3f4f294f59971988
Bódis_Bogi <nyusziful2929@gmail.com>
2026-01-29 09:38:27 +0100

    port

--- api/bookings_create.php
+++ api/bookings_create.php
@@ -4,7 +4,7 @@
 // ===============================
 session_start();
 
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
--- api/register.php
+++ api/register.php
@@ -2,7 +2,7 @@
 // ===============================
 // CORS HEADERS
 // ===============================
-header("Access-Control-Allow-Origin: http://localhost:5174");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
