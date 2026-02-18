e08d02d63b726e53b39fb65977fc143298c00ffd
szalaiszabolcs <szszalai626@gmail.com>
2026-02-18 11:44:00 +0100

    uj

--- api/bookings_create.php
+++ api/bookings_create.php
@@ -48,6 +48,67 @@ $car_year         = (int)$data["car_year"];
 $fuel_type        = $data["fuel_type"];
 $engine_size      = $data["engine_size"] ?? null;
 
+
+//CSAK TESZTELÉSHEZ, AMÍG NINCS RENDESEN MEGCSINÁLVA A SELECT MEZŐK!!!!!!!!!!!!
+$car_model = 1;
+$fuel_type = 1;
+$engine_size = 1;
+
+$stmt = $pdo->prepare("SELECT id FROM vehicles WHERE user_id = :user_id AND fuel_type_id = :fuel_type_id AND engine_size_id = :engine_size_id AND model_id = :model_id");
+
+// Bind parameter
+
+$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$stmt->bindParam(':model_id', $car_model, PDO::PARAM_INT);
+$stmt->bindParam(':fuel_type_id', $fuel_type, PDO::PARAM_INT);
+$stmt->bindParam(':engine_size_id', $engine_size, PDO::PARAM_INT);
+
+// Execute
+$stmt->execute();
+
+// Fetch results
+$vehicleIds = $stmt->fetchAll(PDO::FETCH_COLUMN);
+echo "<pre>";
+print_r($vehicleIds);
+echo "</pre>";
+
+if($vehicleIds) {
+    echo "Van id";
+    try {
+        $stmt = $pdo->prepare("
+            INSERT INTO work_process (
+                vehicle_id,
+                appointment_date,
+                appointment_time
+            ) VALUES (
+                :vehicle_id,
+                :date,
+                :time
+            )
+        ");
+    
+        $stmt->execute([
+            ":date"       => $appointment_date,
+            ":time"       => $appointment_time,
+            ":vehicle_id" => $vehicleIds[0]
+        ]);
+    
+        echo json_encode([
+            "success" => true,
+            "message" => "Sikeres foglalás"
+        ]);
+    
+    } catch (PDOException $e) {
+        echo json_encode([
+            "success" => false,
+            "message" => $e->getMessage()
+        ]);
+    }   
+} else {
+    echo "nincs id";
+}
+/*
+
 // ===============================
 // MENTÉS
 // ===============================
@@ -99,3 +160,9 @@ try {
         "message" => $e->getMessage()
     ]);
 }
+*/
+
+echo json_encode([
+    "success" => false,
+    "message" => $vehicleIds
+]);
